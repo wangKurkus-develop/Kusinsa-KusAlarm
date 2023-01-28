d28592f2cc78fef08ad968a782c2026045f87a26
cwangg897 <dhkdrb897@gmail.com>
2023-01-28 14:03:08 +0900

    init commit

---
+++ src/test/java/com/kurkus/kusalarm/KusalarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurkus.kusalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusalarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
