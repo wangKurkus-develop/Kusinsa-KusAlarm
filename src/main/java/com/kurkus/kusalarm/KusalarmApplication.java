820290091aad384e70eb285810c426ff4fa0449a
Choi Wang Gyu <79621675+cwangg897@users.noreply.github.com>
2023-01-28 14:03:44 +0900

    init commit (#1)

---
+++ src/main/java/com/kurkus/kusalarm/KusalarmApplication.java
@@ -0,0 +1,13 @@
+package com.kurkus.kusalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusalarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusalarmApplication.class, args);
+	}
+
+}
